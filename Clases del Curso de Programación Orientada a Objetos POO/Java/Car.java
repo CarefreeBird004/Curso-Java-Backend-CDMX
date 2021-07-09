98b71430c5a127eab8344d79b006375fc2e07636
Jose Antonio Toledo <antonio_ixtepec@hotmail.com>
2021-07-09 18:55:54 -0500

    Curso de POO, actualizacion

--- Clases del Curso de Programación Orientada a Objetos POO/Java/Car.java
+++ Clases del Curso de Programación Orientada a Objetos POO/Java/Car.java
@@ -1,12 +1,62 @@
 package Java;
 
 class Car {
-    Integer id;
-    String  license;
-    String  driver;
-    Integer passenger;
-     
-    void printDataCar() {
-        System.out.println("License: " + license + " Driver: " +driver);
+    private Integer id;
+    private String  license;
+    private Account  driver;
+    private Integer passenger;
+    
+
+    public Car(String license, Account driver){
+        this.license = license;
+        this.driver = driver;
+        
     }
+
+    void printDataCar() {
+        if (passenger != null){
+            System.out.println("License: " + license + " Driver: " + driver.name+ " El numero de pasagero es:" + passenger);
+
+        }
+    }
+    public Integer getPassenger(){
+        return passenger;
+    }
+    public void setPassenger(Integer passenger){
+        if (passenger==4){
+        this.passenger = passenger;
+        }else{
+            System.out.println("Necesitas asignar 4 passenger");
+        }
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+    public Account getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Account driver) {
+        this.driver = driver;
+    }
+
+
+
+
+
+    
 }
--- Clases del Curso de Programación Orientada a Objetos POO/Java/Main.java
+++ Clases del Curso de Programación Orientada a Objetos POO/Java/Main.java
@@ -3,16 +3,16 @@ package Java;
 class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
-        Car car1 = new Car();
-        car1.license = "AMQ123";
-        car1.driver = "Andres Herrera";
-        car1.passenger = 4;
-        car1.printDataCar();
+        UberX uberX = new UberX("AMQ123", new Account("Andres Herrera Toledo", "AND123"),"Chevrolet","Cehvy");
+        uberX.setPassenger(4); 
+        uberX.printDataCar();
 
-        Car car2 = new Car();
-        car2.license = "QWE576";
-        car2.driver = "Andrea Herrea";
-        car2.passenger = 3;
-        car2.printDataCar();
+       // Car car2 = new Car("QWE576",new Account("Andrea Herrera", "AND1123") );
+        //car2.passenger = 3;
+        //car2.printDataCar();
+        
+        UberVan uberVan = new UberVan("ACSA21", new Account("Andres Toledo", "asd12"));
+        uberVan.setPassenger(6);
+        uberVan.printDataCar();
     }
 }
