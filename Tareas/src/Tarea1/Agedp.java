fdd74ec27fd480e2d6878201b41b4cb853d465e5
Jose Antonio Toledo Gonzalez <antonio_ixtepec@hotmail.com>
2021-07-21 21:41:12 -0500

    Tareas agregadas y actualizacion de cursos

---
+++ Tareas/src/Tarea1/Agedp.java
@@ -0,0 +1,7 @@
+package Tarea1;
+
+public class Agedp {
+    public Agedp(int x){
+
+    }
+}
---
+++ Tareas/src/Tarea1/Kinder.java
@@ -0,0 +1,7 @@
+package Tarea1;
+
+public class Kinder extends Agedp{
+    public Kinder(int x){
+        super(x);
+    }
+}
