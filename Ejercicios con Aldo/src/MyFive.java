16d767de16afba69c069437c9aad9f9cc14799da
Jose Antonio Toledo Gonzalez <antonio_ixtepec@hotmail.com>
2021-08-02 19:40:15 -0500

    Ejercicios con Aldo, agregado

---
+++ Ejercicios con Aldo/src/MyFive.java
@@ -0,0 +1,11 @@
+public class MyFive {
+    public static void main(String[] args) {
+        short kk=11;
+        short ii;
+        short jj = 0;
+        for (ii = kk; ii > 6; ii -= 1) {
+            jj++;
+        }
+        System.out.println("jj = " + jj);
+    }
+}
---
+++ Ejercicios con Aldo/src/Simple.java
@@ -0,0 +1,13 @@
+
+public class Simple{
+
+    public float price;
+
+    public static void main(String[] args){
+
+        Simple price = new Simple();
+
+        price.price = 4;
+    }
+}
+
